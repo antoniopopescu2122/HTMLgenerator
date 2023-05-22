8a81c78aceba23f29568c7359476aefce6af6e9b
Alice Pazitor <alice.pazitor02@e-uvt.ro>
2023-05-22 09:45:51 +0300

    Revert "final version" hello back
    This reverts commit 6cfcfff0b51e2fba44ce18580841aa735940f9fe.

--- modifications.py
+++ modifications.py
@@ -1,82 +1,36 @@
 import json
+import datetime
 
-# Read the current data
-with open('data.json') as file:
-    current_data = json.load(file)
 
-try:
-    # Read the previous data if it exists
-    with open('previous_data.json') as file:
-        previous_data = json.load(file)
-except FileNotFoundError:
-    # If previous_data.json doesn't exist, consider all components as new additions
-    previous_data = {}
+class ModificationsTracker:
+    def __init__(self, modifications_file):
+        self.modifications_file = modifications_file
+        self.modifications = []
 
-# Compare the current and previous data to track modifications
-modifications = {}
-for component, current_component_data in current_data['BINARIES'].items():
-    if component in previous_data:
-        previous_component_data = previous_data[component]
-        modified_fields = {}
-        for field, current_value in current_component_data.items():
-            previous_value = previous_component_data.get(field)
-            if current_value != previous_value:
-                modified_fields[field] = {
-                    'previous_value': previous_value,
-                    'current_value': current_value
-                }
-        if modified_fields:
-            modifications[component] = {
-                'type': 'modified',
-                'details': modified_fields
-            }
-    else:
-        modifications[component] = {
-            'type': 'added',
-            'details': current_component_data
+    def track_modification(self, component, branch, revision):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        modification = {
+            'timestamp': timestamp,
+            'component': component,
+            'branch': branch,
+            'revision': revision
         }
+        self.modifications.append(modification)
 
-# Check for deleted components
-for component in previous_data:
-    if component not in current_data['BINARIES']:
-        modifications[component] = {'type': 'deleted'}
-
-# Save the current data as the previous data for future comparisons
-with open('previous_data.json', 'w') as file:
-    json.dump(current_data['BINARIES'], file)
-#some html
-html = '<html><head>'
-html += '<style>'
-html += 'ul { list-style-type: disc; margin-left: 20px; }'
-html += 'li { margin-bottom: 10px; }'
-html += 'h1 { font-size: 24px; }'
-html += '</style>'
-html += '</head><body>'
-html += '<h1>Modifications</h1>'
-if modifications:
-    html += '<ul>'
-    for component, modification in modifications.items():
-        modification_type = modification['type']
-        html += f'<li><strong>{component}</strong>: '
-        if modification_type == 'added':
-            html += f'Added Component: {modification["details"]}'
-        elif modification_type == 'deleted':
-            html += 'Deleted Component'
-        elif modification_type == 'modified':
-            html += 'Modified Fields:<ul>'
-            for field, details in modification['details'].items():
-                previous_value = details['previous_value']
-                current_value = details['current_value']
-                html += f'<li>{field}: {previous_value} -> {current_value}</li>'
-            html += '</ul>'
-        html += '</li>'
-    html += '</ul>'
-else:
-    html += '<p>No modifications</p>'
-html += '</body></html>'
+    def save_modifications(self):
+        with open(self.modifications_file, 'w') as f:
+            json.dump(self.modifications, f)
 
 
+# Example usage:
+modifications_file = 'modifications.json'
+tracker = ModificationsTracker(modifications_file)
 
-# Save the HTML file
-with open('modifications.html', 'w') as file:
-    file.write(html)
+# Track modifications
+component = input("Component Name: ")
+branch = input("Branch: ")
+revision = input("Revision: ")
+tracker.track_modification(component, branch, revision)
+
+# Save modifications
+tracker.save_modifications()
