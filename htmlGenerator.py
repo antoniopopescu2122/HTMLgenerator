6cfcfff0b51e2fba44ce18580841aa735940f9fe
Alice Pazitor <alice.pazitor02@e-uvt.ro>
2023-05-22 09:11:32 +0300

    final version

--- htmlGenerator.py
+++ htmlGenerator.py
@@ -1,82 +1,85 @@
 import json
-import re
-from xmlgenerator import XMLGenerator
 
+# Read the current data
+with open('data.json') as file:
+    current_data = json.load(file)
 
-class HTMLGenerator:
-    def __init__(self, data_file):
-        self.data_file = data_file
-        self.pattern = r"^[a-z0-9]{40}$"
+try:
+    # Read the previous data if it exists
+    with open('previous_data.json') as file:
+        previous_data = json.load(file)
+except FileNotFoundError:
+    # If previous_data.json doesn't exist, consider all components as new additions
+    previous_data = {}
 
-    def read_json(self):
-        with open(self.data_file) as f:
-            self.data = json.load(f)
-
-    def generate_html(self, output_file):
-        with open(output_file, 'w') as f:
-            f.write('<html><head>')
-            f.write('<link rel="stylesheet" type="text/css" href="style.css">')
-            f.write('</head><body>')
-            f.write('<table>')
-            f.write(
-                '<tr style="background-color:lime;"><td colspan="3" align="center"><b>BINARY</b></td><td></td><td align="center"><b>ECL</b></td></tr>')
-            f.write(
-                '<tr style="background-color:lime;"><th>Name</th><th>Branch</th><th>Revision</th><th>Status</th><th>branch@revision</th></tr>')
-            for component, details in self.data['BINARIES'].items():
-                f.write('<tr>')
-                f.write('<td style="text-transform:uppercase;">' + component + '</td>')
-                f.write('<td>' + details['branch'] + '</td>')
-                if re.match(self.pattern, details['revision']):
-                    f.write('<td><a href="https://github.com/q/commit:' + details['revision'] + '" target="_blank">' +
-                            details['revision'] + '</a></td>')
-                else:
-                    f.write('<td>ERR</td>')
-                f.write('<td>Aligned</td>')
-                if re.match(self.pattern, details['revision']):
-                    f.write('<td><a target="_blank" href="https://github.com/q/commit:' + details['revision'] + '"> ' +
-                            details['branch'] + '@' + details['revision'] + '</a></td>')
-                else:
-                    f.write('<td>ERR</td>')
-                f.write('</tr>')
-            f.write('</table>')
-            f.write('</body></html>')
-
-    def add_entry_to_json(self, component, branch, revision):
-        # Read the existing JSON data
-        with open(self.data_file) as f:
-            data = json.load(f)
-
-        # Add the new entry
-        data['BINARIES'][component] = {
-            'details': {'ECL_revision': '37547155c9a34cd16f6992c05f110154bdcbab92', 'ECL_branch': 'master'},
-            'repository': 'MN/OAM/' + component,
-            'branch': branch,
-            'revision': revision,
-            'artifactory_url_component_specific': 'https://artifactory.com/artifactory/components/component1/master/' + revision
+# Compare the current and previous data to track modifications
+modifications = {}
+for component, current_component_data in current_data['BINARIES'].items():
+    if component in previous_data:
+        previous_component_data = previous_data[component]
+        modified_fields = {}
+        for field, current_value in current_component_data.items():
+            previous_value = previous_component_data.get(field)
+            if current_value != previous_value:
+                modified_fields[field] = {
+                    'previous_value': previous_value,
+                    'current_value': current_value
+                }
+        if modified_fields:
+            modifications[component] = {
+                'type': 'modified',
+                'details': modified_fields
+            }
+    else:
+        modifications[component] = {
+            'type': 'added',
+            'details': current_component_data
         }
 
-        # Save the modifications to the JSON file
-        with open(self.data_file, 'w') as f:
-            json.dump(data, f)
+# Check for deleted components
+for component in previous_data:
+    if component not in current_data['BINARIES']:
+        modifications[component] = {'type': 'deleted'}
 
+# Save the current data as the previous data for future comparisons
+with open('previous_data.json', 'w') as file:
+    json.dump(current_data['BINARIES'], file)
 
-# Example usage with user interface
-data_file = 'data.json'
-output_file = 'output.html'
+# Generate HTML to display the modifications
+html = '<html><head>'
+html += '<style>'
+html += 'ul { list-style-type: none; padding-left: 0; }'
+html += 'li { margin-bottom: 20px; }'
+html += 'h1 { font-size: 24px; }'
+html += '.added { background-color: #d4f5d4; }'
+html += '.deleted { background-color: #f5d4d4; }'
+html += '.modified { background-color: #ffe6b3; }'
+html += '.field { font-weight: bold; }'
+html += '</style>'
+html += '</head><body>'
+html += '<h1>Modifications</h1>'
+if modifications:
+    html += '<ul>'
+    for component, modification in modifications.items():
+        modification_type = modification['type']
+        html += f'<li class="{modification_type}"><strong>{component}</strong>: '
+        if modification_type == 'added':
+            html += f'Added Component: {modification["details"]}'
+        elif modification_type == 'deleted':
+            html += 'Deleted Component'
+        elif modification_type == 'modified':
+            html += 'Modified Fields:<ul>'
+            for field, details in modification['details'].items():
+                previous_value = details['previous_value']
+                current_value = details['current_value']
+                html += f'<li><span class="field">{field}:</span> <span>{previous_value} -> {current_value}</span></li>'
+            html += '</ul>'
+        html += '</li>'
+    html += '</ul>'
+else:
+    html += '<p>No modifications</p>'
+html += '</body></html>'
 
-html_generator = HTMLGenerator(data_file)
-html_generator.read_json()
-html_generator.generate_html(output_file)
-
-# User interface for adding new entry
-
-
-if input("Do you want to add a new entry? (Y/N): ").lower() == 'y':
-    print("Add New Entry")
-    component = input("Component Name: ")
-    branch = input("Branch: ")
-    html_generator.generate_html(output_file)
-
-
-xml_generator = XMLGenerator()
-xml_generator.generate_xml(html_generator.data, "output.xml")
+# Save the HTML file
+with open('modifications.html', 'w') as file:
+    file.write(html)
